3592d54eff9e33e8413483f2aa7a94c75c23d430
rong fengliang <1141591465@qq.com>
2023-03-16 20:05:05 +0800

    format

--- src/main/java/com/dalong/MyExtension.java
+++ src/main/java/com/dalong/MyExtension.java
@@ -3,7 +3,6 @@ package com.dalong;
 import io.pebbletemplates.pebble.extension.AbstractExtension;
 import io.pebbletemplates.pebble.extension.Filter;
 import io.pebbletemplates.pebble.extension.Function;
-import io.pebbletemplates.pebble.tokenParser.AutoEscapeTokenParser;
 import io.pebbletemplates.pebble.tokenParser.TokenParser;
 
 import java.util.ArrayList;
